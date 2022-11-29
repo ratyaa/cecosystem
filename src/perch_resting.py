72396f6e388063dd91219a13186d53d19fc4adf7
Ratibor Siryi <siryi.ra@phystech.edu>
2022-11-29 19:42:47 +0300

    Обновил юмл и добавил набросок классов окуня.

---
+++ src/perch_resting.py
@@ -0,0 +1,14 @@
+import perch
+
+class PerchResting(Perch):
+    def __init__(self, perch):
+        self.pos = perch.pos
+        self.v = perch.v
+        self.a = perch.a
+
+    def __move():
+
+    def activity(self):
+
+    def observe(self, entities):
+    
