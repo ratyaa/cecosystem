260f6caf7857f24c2b6fd3838eca03b43edc1920
Ratibor Siryi <siryi.ra@phystech.edu>
2022-12-06 19:02:06 +0300

    Перегрузил операторы для работы с Coord
    Теперь операции сложения и вычитания векторов, а также произведения на скаляр
    можно производить с помощью операторов +, - и *, как с обычными числами.

--- src/coord.py
+++ src/coord.py
@@ -4,3 +4,15 @@ from dataclasses import dataclass
 class Coord:
     x: float
     y: float
+
+    def __add__(self, other):
+        return Coord(self.x + other.x, self.y + other.y)
+    __radd__ = __add__
+    
+    def __sub__(self, other):
+        return Coord(self.x - other.x, self.y - other.y);
+    __rsub__ = __sub__
+
+    def __mul__(self, factor):
+        return Coord(self.x * factor, self.y * factor)
+    __rmul__ = __mul__
