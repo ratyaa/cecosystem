527bfab8d009e2a06707b13528101940c8c47290
Ratibor Siryi <siryi.ra@phystech.edu>
2022-12-06 19:00:08 +0300

    Пофиксил работу с таймером.

--- src/display_frame.py
+++ src/display_frame.py
@@ -17,11 +17,16 @@ class DisplayFrame(frame.Frame):
         model_old_t = draw_old_t
         draw_delay = 1000.0 / self.framerate
         model_delay = 1000.0 / self.model.get_update_rate()
+        # print_delay = 1000.0
+        # print_old_t = draw_old_t
+        # draw_ticks = 0
+        # model_ticks = 0
         
         while not finished:
             current_t = pygame.time.get_ticks()
             draw_t = current_t - draw_old_t
             model_t = current_t - model_old_t
+            # print_t = current_t - print_old_t
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -30,8 +35,17 @@ class DisplayFrame(frame.Frame):
             if draw_t >= draw_delay:
                 self.model.draw()
                 pygame.display.update()
-                draw_old_t = draw_t
+                draw_old_t = current_t
+                # draw_ticks += 1
 
             if model_t >= model_delay:
                 self.model.update()
-                model_old_t = draw_t
+                model_old_t = current_t
+                # model_ticks += 1
+
+            # if print_t >= print_delay:
+            #     print(draw_ticks, model_ticks)
+            #     print_old_t = current_t
+            #     draw_ticks = 0
+            #     model_ticks = 0
+            
