72761618cfc8b6fb5d6d511161e14894c4864c0d
Sergey Melnikov <serzh.melnikov.16@mail.ru>
2022-12-07 17:37:21 +0300

    Фикшу предыдущий коммит

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ frame = display_frame.DisplayFrame(
     app_config.WIDTH,
     app_config.HEIGHT,
     app_config.FRAME_RATE,
-    model_handler.ModelHandler(app_config.MODEL_RATE)
+model_handler.ModelHandler(app_config.MODEL_RATE)
 )
 
 frame.update()
