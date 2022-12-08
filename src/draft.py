7be2570b6b4ad2a5bab96adc65eebd43179e5ac6
Sergey Melnikov <serzh.melnikov.16@mail.ru>
2022-12-08 23:28:39 +0300

    Вернул обратно многие настройки, которые Ратибор посчитал лишними, багов стало намного меньше

--- src/draft.py
+++
@@ -1,4 +0,0 @@
-if self.pos.x - self.r <= 0 or self.pos.x + self.r >= app_config.WIDTH:
-    self.v.x = -self.v.x
-if self.pos.y - self.r <= 0 or self.pos.x + self.r >= app_config.HEIGHT:
-    self.v.y = -self.v.y
